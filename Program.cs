9f7e4a4adab445e2321e7453720553eb2f92894d
Tommy <tommy-sc@live.no>
2022-11-01 12:46:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sims
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+
+// 3 Modes: FamilyMode, BuildMode and PlayMode
+// User should be able to add familymembers, and those can be either kids or adults
+
+
+// Classes
+
+// GameManager
+// HouseManager
+// FamilyManager
+
+// House
+// Room
+// Floor
+
+// 
